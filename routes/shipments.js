137fcb5fd8e169c2dc82c889c93fc5a42de744d2
Amazonsexpress <garciagilgal@gmail.com>
2026-04-03 02:37:17 -0700

    fix railway routes and controller

--- routes/shipments.js
+++ routes/shipments.js
@@ -1,37 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {
-  getAllShipments,
-  getShipment,
-  createShipment,
-  updateShipment,
-  deleteShipment,
-  trackShipment,
-  addTimelineEvent,
-  updateCustomsStatus,
-  searchShipments
-} = require('../controllers/shipmentController');
+
+const { trackShipment } = require('../controllers/shipmentController');
 
 // Public tracking route
 router.get('/track/:trackingId', trackShipment);
-
-// Admin routes
-router.route('/')
-  .get(getAllShipments)
-  .post(createShipment);
-
-router.route('/search')
-  .get(searchShipments);
-
-router.route('/:id')
-  .get(getShipment)
-  .put(updateShipment)
-  .delete(deleteShipment);
-
-router.route('/:id/timeline')
-  .post(addTimelineEvent);
-
-router.route('/:id/customs')
-  .put(updateCustomsStatus);
 
 module.exports = router;
