2f9a65a351694bb2d8d5e2bb42c955e331118f76
Amazonsexpress <garciagilgal@gmail.com>
2026-04-03 02:57:13 -0700

    fix railway port and root route

--- server.js
+++ server.js
@@ -46,8 +46,11 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
