2eadab52e1247f89cbbb5a697d88c060b385427c
Amazonsexpress <garciagilgal@gmail.com>
2026-04-03 03:05:06 -0700

    fix port binding

--- server.js
+++ server.js
@@ -46,12 +46,12 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Backend is running');
 });
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
 
